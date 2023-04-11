566947f207da5c5ce224689cfc6b29d4a7771aa9
Ramon Kamibayashi <ramonkawa@gmail.com>
2023-04-11 17:22:22 +0200

    added spam control

--- static/contact.php
+++ static/contact.php
@@ -1,10 +1,33 @@
 <?php
+session_start();
+$time_limit = 60;
+
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $name = trim($_POST["name"]);
   $email = trim($_POST["email"]);
   $subject = trim($_POST["subject"]);
   $phone_number = trim($_POST["phone-number"]);
   $message = trim($_POST["message"]);
+  $honeypot = trim($_POST["direccion"]);
+
+  
+  if (!empty($honeypot)) {
+    http_response_code(400);
+    echo "Invalid request.";
+    exit;
+  }
+
+    // Check the time limit for spam
+    $last_submit_time = isset($_SESSION["last_submit_time"]) ? $_SESSION["last_submit_time"] : 0;
+    $num_submissions = isset($_SESSION["num_submissions"]) ? $_SESSION["num_submissions"] : 0;
+    $current_time = time();
+    if (($current_time - $last_submit_time) < $time_limit && $num_submissions >= 5) {
+      // If the time limit has not passed and the number of submissions exceeds the limit, this is likely a spam submission
+      http_response_code(429);
+      echo "Too many submissions. Please try again later.";
+      exit;
+    }
 
   // Check if all required fields are filled out
   if (empty($name) || empty($email) || empty($subject) || empty($message)) {
@@ -81,8 +104,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "Ha ocurrido un error. Por favor, inténtelo de nuevo.";
     exit;
   }
+// Update the last submit time and number of submissions
+$_SESSION["last_submit_time"] = $current_time;
+$_SESSION["num_submissions"] = $num_submissions + 1;
 } else {
-  http_response_code(400);
-  echo "Invalid request.";
-  exit;
+http_response_code(400);
+echo "Invalid request.";
+exit;
 }
